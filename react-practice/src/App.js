d88e6c1e9d29bf23c8a5f10ea8759ef31dec40ba
Himanshu Sangale <himanshu@Himanshus-Air.lan>
2024-01-15 18:50:05 +0530

    useRef

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -2,12 +2,16 @@ import ParentComponent from "./components/ParentComponent";
 
 import "./App.css";
 import Counter from "./components/Counter";
+import FocusInput from "./components/FocusInput";
+import HookTimer from "./components/HookTimer";
 
 function App() {
   return (
     <div className="App">
       {/* <ParentComponent /> */}
-      <Counter />
+      {/* <Counter /> */}
+      {/* <FocusInput /> */}
+      <HookTimer />
     </div>
   );
 }
