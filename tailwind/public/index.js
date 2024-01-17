71f06b545cd8a0e6766a92363029e60ca05a8efb
Himanshu Sangale <himanshu@Himanshus-Air.lan>
2024-01-17 19:09:15 +0530

    TailwindCSS practice

---
+++ tailwind/public/index.js
@@ -0,0 +1,15 @@
+const burger = document.querySelector("#burger");
+const menu = document.querySelector("#menu");
+
+burger.addEventListener("click", () => {
+  //   if (menu.classList.contains("hidden")) {
+  //     menu.classList.remove("hidden");
+  //   } else {
+  //     menu.classList.add("hidden");
+  //   }
+  menu.classList.toggle("hidden");
+  //   menu.classList.toggle("translate-y-full");
+  //   menu.classList.toggle("translate-y-full");
+  //   menu.classList.toggle("opacity-100");
+  //   menu.classList.toggle("bg-white");
+});
