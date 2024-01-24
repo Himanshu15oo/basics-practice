9db4ff871573f00b3f26f56af97d48c71aedd19b
Himanshu Sangale <himanshu@Himanshus-Air.lan>
2024-01-24 10:55:09 +0530

    Include, Require, Require_once

---
+++ include_base.php
@@ -0,0 +1,11 @@
+<?php
+// Including a function from include_fun.php 
+// include('./include_fun.php');
+
+// Requiring a function from include_fun.php 
+// require('./include_fun.php');
+
+// Requiring Once a function from include_fun.php 
+require_once('./include_fun.php');
+
+say_hello("Good Morning!");
